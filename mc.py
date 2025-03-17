f78fa20af3c2514bc6931ca9cfa58e461e3cafb8
cocaobean <carsonzhou2022@gmail.com>
2025-03-17 15:45:32 -0700

    Add money attribute and inventory management to Player class

--- mc.py
+++ mc.py
@@ -6,7 +6,26 @@ class Player:
             health: int = 20,
             attack: int = 2,
             level: int = 1,
-            xp: int = 0
+            xp: int = 0,
+            money: int = 0,
+            inventory: list[list] = [
+                    ["1. Healing Potion", 2], 
+                    ["2. Max Health Potion", 1],
+                    ["3. Strength Potion", 1]
+                ]
             ):
-        pass
+        self.name = name
+        self.max_health = max_health
+        self.health = health
+        self.attack = attack
+        self.level = level
+        self.xp = xp
+        self.money = money
+        self.inventory = inventory
 
+    def print_inventory(self) -> None:
+        for item, quantity in self.inventory:
+            if quantity == 0:
+                continue    # skip
+
+            print(item, ":", str(quantity), "left")
